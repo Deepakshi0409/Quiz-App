249970ab5c54d11655664df4d3ed22b244965ac2
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2020-12-04 11:32:48 +0530

    added sorting order

--- app/src/main/java/com/example/askme/StateViewModel.java
+++ app/src/main/java/com/example/askme/StateViewModel.java
@@ -3,8 +3,11 @@ package com.example.askme;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.arch.core.util.Function;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.paging.PagedList;
 
 import com.example.askme.Data.States;
@@ -13,12 +16,22 @@ import com.example.askme.Database.StatesRepository;
 public class StateViewModel extends AndroidViewModel {
     private StatesRepository statesRepository;
     public LiveData<PagedList<States>> datas;
+
+    public MutableLiveData<String> sortOrderChanged =  new MutableLiveData<>();
     public StateViewModel(@NonNull Application application) {
         super(application);
 
         statesRepository = StatesRepository.getStatesRepository(application);
-        datas = statesRepository.getAllStates();
+        sortOrderChanged.setValue("statesId");
+        datas = Transformations.switchMap(sortOrderChanged, new Function<String, LiveData<PagedList<States>>>() {
+            @Override
+            public LiveData<PagedList<States>> apply(String input) {
+                return statesRepository.getallstates(input);
+            }
+        });
+
     }
+    public void changesortingOrder(String sortBy){sortOrderChanged.postValue(sortBy);}
     public void inseretStates(States states) {
         statesRepository.insertStates(states);
     }
