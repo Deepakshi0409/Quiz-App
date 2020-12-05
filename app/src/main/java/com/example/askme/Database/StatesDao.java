7d97c00af504404c5436da63e02ea47dba57f4cc
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2020-12-05 12:05:10 +0530

    added a query method

--- app/src/main/java/com/example/askme/Database/StatesDao.java
+++ app/src/main/java/com/example/askme/Database/StatesDao.java
@@ -5,9 +5,13 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.RawQuery;
 import androidx.room.Update;
+import androidx.sqlite.db.SupportSQLiteQuery;
 
 import com.example.askme.Data.States;
+
+import java.util.List;
 
 @Dao
 public interface StatesDao {
@@ -25,4 +29,10 @@ public interface StatesDao {
 
     @Query("Select * from State ORDER BY RANDOM() LIMIT 1")
     States getStateForNotification();
+
+    @RawQuery(observedEntities = States.class)
+    DataSource.Factory<Integer,States> getAllSortedStates(SupportSQLiteQuery query);
+
+    @Query("SELECT DISTINCT * FROM  State ORDER BY RANDOM() LIMIT 4")
+    List<States> getQuizStates();
 }
