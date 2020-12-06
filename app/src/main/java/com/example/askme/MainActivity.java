dd266df5fcd706928807c04bc22a071bdece5e2e
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2020-12-06 18:04:09 +0530

    quiz update

--- app/src/main/java/com/example/askme/MainActivity.java
+++ app/src/main/java/com/example/askme/MainActivity.java
@@ -2,21 +2,70 @@ package com.example.askme;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.example.askme.Settings.Settings;
+import com.example.askme.Custom.QuizView;
+import com.example.askme.Custom.QuizViewModel;
+import com.example.askme.Data.States;
+import com.example.askme.Settings.settingsActivity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuizView quizView;
+    private QuizViewModel quizViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+        quizView = findViewById(R.id.quizView);
+        quizViewModel.data.observe(this, new Observer<List<States>>() {
+            @Override
+            public void onChanged(List<States> states) {
+                if (states!=null){
+                    if (states.size()==4){
+                        quizView.setData(states);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"Add more States",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        quizView.setOptionsClickListner(new QuizView.OptionsClickListner() {
+            @Override
+            public void optionsClick(Boolean result) {
+                updateResult(result);
+            }
+        });
+
+    }
+
+    private void updateResult(Boolean result) {
+        if (result){
+            Toast.makeText(MainActivity.this,"Correct Answer",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(MainActivity.this,"InCorrect Answer",Toast.LENGTH_SHORT).show();
+        }
+        quizViewModel.refreshGame();
+        quizView.reset();
+
     }
 
     @Override
@@ -37,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.settings:
-                Intent intent1 = new Intent(MainActivity.this, Settings.class);
+                Intent intent1 = new Intent(MainActivity.this, settingsActivity.class);
                 startActivity(intent1);
                 return true;
 
